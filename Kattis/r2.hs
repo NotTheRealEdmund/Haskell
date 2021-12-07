233f912033b1c8185738f38dfba6656822bc1adc
NotTheRealEdmund <nottherealedmund@gmail.com>
2021-12-07 16:20:50 +0800

    Create r2.hs

---
+++ Kattis/r2.hs
@@ -0,0 +1,12 @@
+main = do
+  a <- getLine
+  -- (words a) will delimit a via space into a list of items
+  -- if a is "2 3", will be delimited into ["2"," ","3"]
+  let b = head (words a) -- "2"
+  let c = last (words a) -- "3"
+  let x = convertInt b   -- 2
+  let y = convertInt c   -- 3
+  print ( (2*y) - x )
+  
+convertInt :: String -> Int
+convertInt n = read n
